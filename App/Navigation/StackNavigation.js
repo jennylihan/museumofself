6e13c84053b681710bb18017fdbba2a9bb3f5a07
jennylihan <jlhan@stanford.edu>
2018-02-21 23:24:34 -0800

    showcase screen

--- App/Navigation/StackNavigation.js
+++ App/Navigation/StackNavigation.js
@@ -2,11 +2,13 @@ import { StackNavigator } from 'react-navigation'
 
 import HomeScreen from '../Screens/HomeScreen'
 import CreateContentScreenStep2 from '../Screens/CreateContentScreenStep2'
+import ShowcaseScreen from '../Screens/ShowcaseScreen'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
   HomeScreen: { screen: HomeScreen},
   CreateContentScreenStep2: {screen: CreateContentScreenStep2},
+  ShowcaseScreen: {screen: ShowcaseScreen},
 }, {
   // Default config for all screens
   headerMode: 'float',
