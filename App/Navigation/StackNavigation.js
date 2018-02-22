7b0992378abd0a73eadb20574dfc72dbb1d76b00
jennylihan <jlhan@stanford.edu>
2018-02-21 23:04:49 -0800

    skeleton of museum app

--- App/Navigation/StackNavigation.js
+++ App/Navigation/StackNavigation.js
@@ -1,20 +1,19 @@
 import { StackNavigator } from 'react-navigation'
 
-import CreateContentScreen from '../Screens/CreateContentScreen'
+import HomeScreen from '../Screens/HomeScreen'
 import CreateContentScreenStep2 from '../Screens/CreateContentScreenStep2'
-import PickerScreen from '../Screens/PickerScreen'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  CreateContentScreen: { screen: CreateContentScreen},
-  CreateContentScreenStep2: {screen: CreateContentScreenStep2}
+  HomeScreen: { screen: HomeScreen},
+  CreateContentScreenStep2: {screen: CreateContentScreenStep2},
 }, {
   // Default config for all screens
   headerMode: 'float',
-  initialRouteName: 'CreateContentScreen',
+  initialRouteName: 'HomeScreen',
   navigationOptions: ({navigation}) => {
 
-    let navTitle = 'Create Content'
+    let navTitle = 'Welcome'
     if (navigation.state.params) {
       navTitle = navigation.state.params.title || navTitle;
     }
